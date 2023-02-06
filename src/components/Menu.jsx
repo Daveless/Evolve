6fead518f148f1d491a4e0dd9a7ae940e185c19d
daveless <davidalrojas@gmail.com>
2023-02-06 10:18:20 -0500

    something

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,11 +8,23 @@ const Menu = () => {
             <div className={style.menuLogo}>
               <p>EVOLVE</p>
             </div>
-            <ul>
+
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="36" height="36" viewBox="0 0 24 24" stroke-width="2.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <line x1="4" y1="6" x2="20" y2="6" />
+  <line x1="4" y1="12" x2="20" y2="12" />
+  <line x1="4" y1="18" x2="20" y2="18" />
+</svg>
+
+        <div className={style.mobileMenu}>
+        <ul>
                 <li><a href="#">Inicio</a></li>
                 <li><a href="#">Cursos</a></li>
                 <li><a href="#">Sobre Nosotros</a></li>
-            </ul>
+        </ul>
+        </div>
+
+            
         </nav>
     </div>
   )
